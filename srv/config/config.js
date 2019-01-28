62644165c11c0bfcce1c844d4d57b4b42473e91a
Chris Whealy <chris.whealy@sap.com>
2019-01-28 16:39:55 +0000

    Add config file

---
+++ srv/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+  development : {
+    refresh_freq: "daily"
+  }
+, production : {
+    refresh_freq: "daily"
+  }
+}
+
+module.exports.config = config
--- srv/server.js
+++ srv/server.js
@@ -1,20 +1,11 @@
-// const cds = require('@sap/cds')
+/*eslint comma-dangle: [2, "only-multiline"]*/
 
-// var colNames = "GeonameId,Name,Latitude,Longitude,FeatureClass,FeatureCode,CountryCode,CountryCodesAlt,Admin1,Admin2,Admin3,Admin4,Population,Elevation,DEM,Timezone,LastModified"
+'strict mode'
 
-// var tableRow = [
-//   2986043,'Pic de Font Blanca',42.64991,1.53335,'T','PK','AD',null,'00',null,null,null,0,null,2860,'Europe/Andorra','2014-11-05'
-// ]
-
-// cds.connect()
-
-// cds.run(
-//   `upsert ORG_GEONAMES_GEONAMES (${colNames}) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) with primary key`,
-//   tableRow
-// )
-
-const http = require('http')
-const bfu  = require('basic-formatting-utils')
+const cds    = require('@sap/cds')
+const http   = require('http')
+const bfu    = require('basic-formatting-utils')
+const config = require('./config/config.js')[process.env.NODE_ENV]
 
 const vcap_app = JSON.parse(process.env.VCAP_APPLICATION)
 const port     = process.env.PORT || 3000
@@ -37,6 +28,21 @@ const server = http.createServer((req, res) => {
   res.end(resp_html)
 })
 
-server.listen(port, () => console.log(`Server running at ${vcap_app.uris[0]}:${port}/`))
+server.listen(port, () => console.log(`Server running at https://${vcap_app.uris[0]}:${port}/`))
 
+// var colNames = "GeonameId,Name,Latitude,Longitude,FeatureClass,FeatureCode,CountryCode,CountryCodesAlt,Admin1,Admin2,Admin3,Admin4,Population,Elevation,DEM,Timezone,LastModified"
+
+// var tableRow = [
+//   2986043,'Pic de Font Blanca',42.64991,1.53335,'T','PK','AD',null,'00',null,null,null,0,null,2860,'Europe/Andorra','2014-11-05'
+// ]
+
+//cds.connect()
+
+// var descriptions = cds.run(`select DESCRIPTION from ORG_GEONAMES_BASE_FEATURE_CLASSES`)
+// console.log(descriptions)
+
+// cds.run(
+//   `upsert ORG_GEONAMES_GEONAMES (${colNames}) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) with primary key`,
+//   tableRow
+// )
 
